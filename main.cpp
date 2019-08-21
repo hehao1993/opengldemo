f08b8f333fe20a41cef0ffb927f42bf74fae1e14
hehao1993 <347428365@qq.com>
2019-08-21 18:13:35 +0800

    添加stb_image.h

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 
 #include <iostream>
 #include "shader_s.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
