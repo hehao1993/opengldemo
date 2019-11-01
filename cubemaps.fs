212af487034f5798e99c36774274aaee6294cb62
hehao1993 <347428365@qq.com>
2019-11-01 11:53:01 +0800

    cube_maps

--- cubemaps.fs
+++ cubemaps.fs
@@ -10,6 +10,10 @@ uniform samplerCube skybox;
 void main()
 {    
     vec3 I = normalize(Position - cameraPos);
-    vec3 R = reflect(I, normalize(Normal));
+	//∑¥…‰
+    //vec3 R = reflect(I, normalize(Normal));
+	//’€…‰
+	float ratio = 1.00 / 1.52;
+    vec3 R = refract(I, normalize(Normal), ratio);
     FragColor = vec4(texture(skybox, R).rgb, 1.0);
 }
